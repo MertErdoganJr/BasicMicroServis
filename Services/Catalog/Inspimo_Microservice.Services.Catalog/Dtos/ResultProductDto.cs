1fe2a52038a0e929ba3a454f55e1c7be64abbc69
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-07-18 19:45:07 +0300

    Add project files.

---
+++ Services/Catalog/Inspimo_Microservice.Services.Catalog/Dtos/ResultProductDto.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Inspimo_Microservice.Services.Catalog.Models;
+
+namespace Inspimo_Microservice.Services.Catalog.Dtos
+{
+    public class ResultProductDto
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string PictureURL { get; set; }
+        public string CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Services/Catalog/Inspimo_Microservice.Services.Catalog/Settings/Abstract/IDataBaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Inspimo_Microservice.Services.Catalog.Settings.Abstract
+{
+    public interface IDataBaseSettings
+    {
+        public string CategoryCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DataBaseName { get; set; }
+    }
+}
